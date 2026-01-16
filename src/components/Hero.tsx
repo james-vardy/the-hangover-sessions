4f7b78936a44e73bff4951b39c76ad2f38ff0c6b
james <james.vardyc@gmail.com>
2026-01-16 15:50:35 +0000

    change file title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
         playsInline
         className="absolute inset-0 w-full h-full object-cover"
       >
-        <source src="/hero/video.MP4" type="video/mp4" />
+        <source src="/hero/video.mp4" type="video/mp4" />
       </video>
 
       {/* Dark overlay for readability */}
